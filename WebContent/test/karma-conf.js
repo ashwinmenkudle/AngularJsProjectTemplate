2dcaa3c7273afc11436da97413391e8e4020280d
Ashwin Menkudle <ashwinmenkudle@gmail.com>
2015-07-25 23:26:38 +0530

    Formatted file

--- WebContent/test/karma-conf.js
+++ WebContent/test/karma-conf.js
@@ -1,65 +1,60 @@
-module.exports = function(config){
-  config.set({
-    //  root path location that will be used to resolve all relative paths in files and exclude sections
-    basePath : '../',
+module.exports = function(config) {
+	config.set({
+		// root path location that will be used to resolve all relative paths in
+		// files and exclude sections
+		basePath : '../',
 
-    // files to include, ordered by dependencies
-    files : [
-      // include relevant Angular files and libs
-      'app/components/angular/angular.js',
-      'app/components/angular-mocks/angular-mocks.js',
-      
-      // include JS files
-      'app/js/**/*.js',
-      'app/js/app.js',
-    
-      // include html template files
-      // 'app/partials/directives/*.html',
-      // 'app/partials/*.html',
+		// files to include, ordered by dependencies
+		files : [
+		// include relevant Angular files and libs
+		'app/components/angular/angular.js',
+				'app/components/angular-mocks/angular-mocks.js',
 
-      // include unit test specs
-      'test/unit/*.js'
-    ], 
-    // files to exclude
-    exclude : [
-      'app/components/angular/angular-loader.js'
-      , 'app/components/angular/*.min.js'
-      , 'app/components/angular/angular-scenario.js'
-    ],
+				// include JS files
+				'app/js/**/*.js', 'app/js/app.js',
 
-    // karma has its own autoWatch feature but Grunt watch can also do this
-    autoWatch : false,
+				// include html template files
+				// 'app/partials/directives/*.html',
+				// 'app/partials/*.html',
 
-    // testing framework, be sure to install the correct karma plugin
-    frameworks: ['jasmine'],
+				// include unit test specs
+				'test/unit/*.js' ],
+		// files to exclude
+		exclude : [ 'app/components/angular/angular-loader.js',
+				'app/components/angular/*.min.js',
+				'app/components/angular/angular-scenario.js' ],
 
-    // browsers to test against, be sure to install the correct browser launcher plugins
-    browsers : ['PhantomJS'],
+		// karma has its own autoWatch feature but Grunt watch can also do this
+		autoWatch : false,
 
-    // map of preprocessors that is used mostly for plugins
-    preprocessors: {
-      // test coverage
-      'app/js/*.js': ['coverage'],
-      'app/js/*.js': ['coverage'],
-    },
+		// testing framework, be sure to install the correct karma plugin
+		frameworks : [ 'jasmine' ],
 
-    reporters: ['progress', 'coverage'],
+		// browsers to test against, be sure to install the correct browser
+		// launcher plugins
+		browsers : [ 'PhantomJS' ],
 
-    // list of karma plugins
-    plugins : [
-      'karma-coverage',
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-jasmine',
-      'karma-phantomjs-launcher'
-    ],
+		// map of preprocessors that is used mostly for plugins
+		preprocessors : {
+			// test coverage
+			'app/js/*.js' : [ 'coverage' ],
+			'app/js/*.js' : [ 'coverage' ],
+		},
 
-    coverageReporter: {
-        type : 'html',
-        dir : 'coverage/'
-    },
-    
-    junitReporter: {
-      outputFile: 'test-results/junit-results.xml'
-    }
-})}
+		reporters : [ 'progress', 'coverage' ],
+
+		// list of karma plugins
+		plugins : [ 'karma-coverage', 'karma-chrome-launcher',
+				'karma-firefox-launcher', 'karma-jasmine',
+				'karma-phantomjs-launcher' ],
+
+		coverageReporter : {
+			type : 'html',
+			dir : 'coverage/'
+		},
+
+		junitReporter : {
+			outputFile : 'test-results/junit-results.xml'
+		}
+	});
+};
